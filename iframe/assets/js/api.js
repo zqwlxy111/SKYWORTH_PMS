c5cd866b1aa07d5e97286ba87236b17ae34cfb89
libinglin <libinglin@skyworth.com>
2019-07-02 17:59:32 +0800

    2019-07-02

--- iframe/assets/js/api.js
+++ iframe/assets/js/api.js
@@ -13,7 +13,7 @@
             util.request2("/project/getList", e, cb);
         },
         startProject: function (e, cb) {
-            util.request("project/startFlow", e, cb);
+            util.request2("project/startFlow", e, cb);
         },
         getPerson: function (e, cb) {
             util.request("project/getPerson", e, cb);
@@ -21,6 +21,10 @@
         getAllPerson: function (e, cb) {
             util.request2("project/getAllPerson", e, cb);
         },
+        setMachineDuty:function(e,cb){
+            console.log("project/setMachineDuty/"+e.projectid+"/"+e.machineid);
+            util.request2("project/setMachineDuty/"+e.projectid+"/"+e.machineid, e.dutys, cb);
+        }
     },
     machine: {
         addMachine: function (e, cb) {
@@ -35,14 +39,21 @@
         getListByProject: function (e, cb) {
             util.request("/machine/getMachineByProjectId", e, cb);
         },
+        getMachineById: function (e, cb) {
+            util.request("/machine/getMachineById", e, cb);
+        },
     },
     flow: {
         getMyFlow: function (cb) {
             util.request("flow/getMyFlow", {}, cb);
         },
         getFlowById: function (e, cb) {
-            util.request("flow/getFlowById", e,cb);
+            util.request("flow/getFlowById", e, cb);
         },
+        getFlowModule: function (e, cb) {
+            util.request("flow/getList", e, cb);
+        },
+
         commit: function (e, cb) {
             util.request2("flow/commit", e, cb);
         },
@@ -52,11 +63,13 @@
         getNodeTasks: function (e, cb) {
             util.request2("/milepost/getList", e, cb);
         },
-        startFileTask: function (e, cb) {
-            util.request("milepost/commit", e, cb);
-        },
-        nodeFileUpload: function (e, cb) {
-            util.uploadFile(e, cb);
+        // startFileTask: function (e, cb) {
+        //     util.request("milepost/commit", e, cb);
+        // },
+        nodeFileUpload: function (target,e, cb) {
+
+            util.uploadFile2(target,e, cb)
+            //util.uploadFile(e, cb);
         },
         downLoadNodeFile: function (e, cb) {
             util.downLoad(e, cb);
@@ -78,16 +91,40 @@
         },
         getFlowByRecord: function (e, cb) {
             util.request("flow/getFlowByRecord", e, cb);
+        },
+        setFileEx: function (e, cb) {
+            util.request("mileport/setFileEx", e, cb);
         }
     },
     node: {
-        getDutys: function (cb) {
-            util.request("node/getDutys", {}, cb);
+        getDutys: function (e,cb) {
+            util.request("node/getDutys", e, cb);
+        },
+        getList: function (e,cb) {
+            util.request("node/getList", e, cb);
+        },
+        deleteNode: function (e, cb) {
+            util.request("node/deleteNodeTask", e, cb);
+        },
+        addNodeTask: function (e, cb) {
+            util.request("/node/addNodeTask", e, cb);
+        },
+        updateNode: function (e, cb) {
+            util.request("/node/update", e, cb);
+        },
+        getWorkNodes:function(e,cb){
+            util.request2("/node/getWorkNodes",e,cb);
+        },
+        saveAll:function(e,cb){
+            util.request2("/node/saveAll",e,cb);
         }
     },
-    task:{
-        getList:function(e,cb){
+    task: {
+        getList: function (e, cb) {
             util.request2("task/getList", e, cb);
         },
+        getMyTask: function (cb) {
+            util.request("task/getMyTask", {}, cb);
+        }
     }
 }
